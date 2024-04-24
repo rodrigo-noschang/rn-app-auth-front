03e997fec5aa04e8d569de1c343d19d715459e2f
Rodrigo Noschang <rodrigo.riderize@gmail.com>
2024-04-24 11:25:39 -0300

    bypass code route and return random codew

--- scripts/consent.js
+++ scripts/consent.js
@@ -15,11 +15,11 @@ const authBtn = document.querySelector('#consent-to-auth');
 const cancelBtn = document.querySelector('#cancel-auth');
 
 async function giveConsent() {
-  const { code } = await fetch(`http://${IP_ADDRESS}:3033/auth/code?redirect_uri=${redirect_uri}&client_id=${client_id}&state=${state}`)
-    .then(res => res.json());
+  // const { code } = await fetch(`http://${IP_ADDRESS}:3033/auth/code?redirect_uri=${redirect_uri}&client_id=${client_id}&state=${state}`)
+  //   .then(res => res.json());
 
   // &state=${state}&client_id=${client_id}&response_type=${response_type}&nonce=${nonce}&code_challenge=${code_challenge}&code_challenge_method=${code_challenge_method}
-  window.location.href = `${redirect_uri}/?code=${code}&state=${state}`;
+  window.location.href = `${redirect_uri}/?code=${'123456'}&state=${state}`;
   // window.location.href = `https://com.opencyclingtracker://auth`;
 
 }
